5e951b8c77eb4c713d694a4a402389d952de4a2f
Ilya-Cherevko <ya.titanikum@yandex.ru>
2022-08-16 22:19:42 +0400

    Верстка

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,22 +4,24 @@ function SearchForm({ isShort, setIsShort, searchQuery, setSearchQuery, searchFi
   return (
     <section className="search">
       <form className="search__form from-search" onSubmit={searchFilms}>
-        <input
-          className="from-search__input"
-          type="text"
-          placeholder='Фильм'
-          value={searchQuery}
-          onInput={e => setSearchQuery(e.target.value)}
-        />
-        <button className="from-search__button" type="button"></button>
+        <div className="from__ellips">
+          <input
+            className="from-search__input"
+            type="text"
+            placeholder='Фильм'
+            value={searchQuery}
+            onInput={e => setSearchQuery(e.target.value)}
+          />
+          <button className="from-search__button" type="button"></button>
+        </div>
         <label className="from-search__label" >
+          <p className="form-search__label-text">Короткометражки</p>
           <input
             className="from-search__checkbox"
             type="checkbox"
             checked={isShort}
             onChange={() => setIsShort(!isShort)}
           />
-          <p className="form-search__label-text">Короткометражки</p>
           <div className="from-search__custom-checkbox">
             <div className="from-search__custom-mark"></div>
           </div>
